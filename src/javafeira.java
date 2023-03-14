4f48e6b14257a8e99628b0d65ef0ec7a7f73a060
LUCAS DE CARVALHO BARREIRO <lucas.barreiro@alunos.sc.senac.br>
2023-03-13 21:23:58 -0300

    Initial commit

---
+++ src/javafeira.java
@@ -0,0 +1,10 @@
+public class javafeira {
+    public static void main(String[] args) {
+
+        double maça = 7.25;
+        double laranja = 5.50 * 3;
+
+        System.out.println(maça + laranja);
+
+    }
+}

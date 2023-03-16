a0731a247aa6d62db00b891183c536f9948ed61e
FaculdadeLucasBarreiro <lucas.barreiro@alunos.sc.senac.br>
2023-03-15 21:56:02 -0300

    .

---
+++ src/exemplos/Saudacao.java
@@ -0,0 +1,16 @@
+package exemplos;
+
+import java.util.Scanner;
+
+public class Saudacao {
+
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("digite uma mensagem: ");
+        String mensagem = teclado.nextLine();
+        System.out.println(mensagem);
+        teclado.close();
+
+    }
+}

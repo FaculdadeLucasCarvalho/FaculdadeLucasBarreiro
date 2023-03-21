ffd9a09f1408ed3683b8acb7dd0bb064f6f2f63b
FaculdadeLucasBarreiro <lucas.barreiro@alunos.sc.senac.br>
2023-03-20 21:44:54 -0300

    .

--- src/exemplos/Calculos.java
+++ src/exemplos/Calculos.java
@@ -4,5 +4,20 @@ public class Calculos {
 
     public static void main(String[] args) {
 
+        int numero1 = 3;
+        int numero2 = 6;
+
+        double resultadoDivisao = 1.0 * numero1 / numero2;
+
+        System.out.println("Soma: " + (numero1 + numero2));
+        System.out.println("Subtração: " + (numero1 - numero2));
+        System.out.println("Multiplicação: " + (numero1 * numero2));
+        System.out.println("Divisão: " + resultadoDivisao);
+
+        System.out.println(++numero1);
+        System.out.println(numero1++);
+        System.out.println(numero1);
+
+        System.out.println("Resto da divisão: " + numero1 % 2);
     }
 }
---
+++ src/exercicios/Feira.java
@@ -0,0 +1,42 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class Feira {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+         double precoMaca;
+         double quiloMaca;
+         double quiloLaranja;
+         double precoLaranja;
+
+        System.out.println("Quantos quilos de Maça? ");
+        quiloMaca = scan.nextDouble();
+
+        System.out.println("Quantos quilos de Laranja? ");
+        quiloLaranja = scan.nextDouble();
+
+        System.out.println("Valor do quilo da Maça? ");
+        precoMaca = scan.nextDouble();
+
+        System.out.println("Valor do quilo da laranja? ");
+        precoLaranja = scan.nextDouble();
+
+        System.out.println("Quilo da Maça " + quiloMaca );
+        System.out.println("Preço da Maça " + precoMaca);
+        System.out.println("Quilo da laraja " + quiloLaranja);
+        System.out.println("Preço da Laranja " + precoLaranja);
+
+        double totalLaranja = (quiloLaranja * precoLaranja);
+        double totalMaca = (quiloMaca * precoMaca);
+
+        System.out.println("Valor gasto na feira: " + (totalMaca + totalLaranja));
+
+
+
+
+
+
+    }
+}

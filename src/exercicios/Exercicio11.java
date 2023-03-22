4cc0b2fcc059b206790fd08981ee4bef354544be
FaculdadeLucasBarreiro <lucas.barreiro@alunos.sc.senac.br>
2023-03-22 20:04:41 -0300

    .

---
+++ src/exercicios/Exercicio11.java
@@ -0,0 +1,17 @@
+package exercicios;
+
+public class Exercicio11 {
+    public static void main(String[] args) {
+        System.out.println("Media final do aluno:");
+
+        double nota1 = 2;
+        double nota2 = 3;
+        double nota3 = 5;
+
+        double total = nota1 + nota2 + nota3;
+        double Exercicio11 = total / 3;
+        System.out.println(Exercicio11);
+
+    }
+
+}

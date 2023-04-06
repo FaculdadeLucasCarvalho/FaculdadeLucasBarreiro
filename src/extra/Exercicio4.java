d6ec14d5b8094e8b8b7e731b366bd131656fa6ea
FaculdadeLucasBarreiro <lucas.barreiro@alunos.sc.senac.br>
2023-04-05 21:22:32 -0300

    .

---
+++ src/extra/Exercicio4.java
@@ -0,0 +1,24 @@
+package extra;
+
+import java.util.Scanner;
+
+public class Exercicio4 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int altura;
+        int sexo;
+        int peso;
+
+        System.out.println("Informe se voce e homem ou mulher \n" +
+                "1 - homem\n" +
+                "2 - mulher\n");
+        sexo = scan.nextInt();
+
+
+
+
+    }
+}

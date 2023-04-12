691b273da102cc4fca1d4a01a8577c70c2a9ce5d
FaculdadeLucasBarreiro <lucas.barreiro@alunos.sc.senac.br>
2023-04-12 19:34:35 -0300

    .

---
+++ src/extra/Exercicio2.java
@@ -0,0 +1,36 @@
+package extra;
+
+import java.util.Scanner;
+
+public class Exercicio2 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        double salarioBruto, salarioLiquido, inss;
+
+        System.out.println("Informe o seu salário bruto: ");
+        salarioBruto = scan.nextDouble();
+
+        inss = salarioBruto * 0.1;
+        salarioLiquido = salarioBruto - inss;
+        double impostoRenda;
+
+        if (salarioLiquido < 1000) {
+            impostoRenda = salarioLiquido * 0.05;
+        } else if (salarioLiquido >= 1000 && salarioLiquido <= 3000) {
+            impostoRenda = salarioLiquido * 0.1;
+        } else {
+            impostoRenda = salarioLiquido * 0.2;
+        }
+
+        salarioLiquido -= impostoRenda;
+
+        System.out.println("O seu salário líquido é: R$ " + salarioLiquido);
+
+
+    }
+
+}
+}
